8766ff3d3c7b3ebc8dab6cf7e2854269ed0abc80
Aron <aronmn@vgy.se>
2016-10-28 14:50:03 +0200

    Övning 1-2

--- Slump/Program.cs
+++ Slump/Program.cs
@@ -10,6 +10,22 @@ namespace Slump
     {
         static void Main(string[] args)
         {
+            //Övning 1
+
+            Console.WriteLine("Programmet slumpar fram 10 tal mellan 1-20");
+            Random slump = new Random();
+            var sum = 0;
+            for (int i = 0; i < 10; i++) 
+            {
+                int tal = slump.Next(21);
+                Console.WriteLine(tal + " ");
+                sum = sum + tal;
+            }
+            Console.WriteLine("Medelvärdet är " + (sum/10));
+
+            //Övning 2
+
+
         }
     }
 }
